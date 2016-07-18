838ea6cdd4580a1f2f5fca255dc669af59c0f7d4
Evan Gipson <evangipson@gmail.com>
2016-07-17 19:32:51 -0500

    Ensure area charts don't bleed into each other

--- js/index.js
+++ js/index.js
@@ -937,7 +937,7 @@ var MICROSTOCKS = (function () {
       width: $("#graph-wrapper").outerWidth(),
       height: $("#graph-wrapper").outerHeight() - 50,
       pointSize: 5,
-      isStacked: false
+      isStacked: true
     };
     var chart = new google.visualization.AreaChart(document.getElementById('graph-wrapper'));
     chart.draw(google.visualization.arrayToDataTable(gameData[type]), options);
